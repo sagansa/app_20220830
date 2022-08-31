9fcfc2313b874494246020ba6838c0e679c51243
sagansa <asapanganbangsa@gmail.com>
2022-08-31 08:00:59 +0700

    update

--- resources/views/app/roles/form-inputs.blade.php
+++ resources/views/app/roles/form-inputs.blade.php
@@ -1,58 +1,44 @@
 @php $editing = isset($role) @endphp
 
 <div class="mt-6 space-y-6 sm:mt-5 sm:space-y-5">
-    <x-input.text
-        name="name"
-        label="Name"
-        value="{{ old('name', ($editing ? $role->name : '')) }}"
-    ></x-input.text>
+    <x-input.text name="name" label="Name" value="{{ old('name', $editing ? $role->name : '') }}"></x-input.text>
 
     <div class="px-4 my-4">
-        <h4 class="font-bold text-lg text-gray-700">
+        <h4 class="text-lg font-bold text-gray-700">
             Assign @lang('crud.permissions.name')
         </h4>
 
-        <div class="py-2">
+        <ul role="list" class="grid grid-cols-2 gap-x-4 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
             @foreach ($permissions as $permission)
-            <div>
-                <x-inputs.checkbox
-                    id="permission{{ $permission->id }}"
-                    name="permissions[]"
-                    label="{{ ucfirst($permission->name) }}"
-                    value="{{ $permission->id }}"
-                    :checked="isset($role) ? $role->hasPermissionTo($permission) : false"
-                    :add-hidden-value="false"
-                ></x-inputs.checkbox>
-            </div>
+                <div>
+                    <x-inputs.checkbox id="permission{{ $permission->id }}" name="permissions[]"
+                        label="{{ ucfirst($permission->name) }}" value="{{ $permission->id }}" :checked="isset($role) ? $role->hasPermissionTo($permission) : false"
+                        :add-hidden-value="false"></x-inputs.checkbox>
+                </div>
+                {{-- <li class="relative">
+                <div
+                    class="block w-full overflow-hidden bg-gray-100 rounded-lg group aspect-w-10 aspect-h-7 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                    <img src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80"
+                        alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+                    <button type="button" class="absolute inset-0 focus:outline-none">
+                        <span class="sr-only">View details for IMG_4985.HEIC</span>
+                    </button>
+                </div>
+                <p class="block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none">IMG_4985.HEIC</p>
+                <p class="block text-sm font-medium text-gray-500 pointer-events-none">3.9 MB</p>
+            </li> --}}
             @endforeach
-        </div>
-    </div>
+            <!-- More files... -->
+        </ul>
 
-    @if ($editing)
-    <x-shows.dl>
-        <x-shows.sub-dl>
-            <x-shows.dt>Created Date</x-shows.dt>
-            <x-shows.dd>{{ $role->created_at }} </x-shows.dd>
-        </x-shows.sub-dl>
-        <x-shows.sub-dl>
-            <x-shows.dt>Updated Date</x-shows.dt>
-            <x-shows.dd>{{ $role->updated_at }} </x-shows.dd>
-        </x-shows.sub-dl>
-        @role('super-admin|manager|supervisor')
-        <x-shows.sub-dl>
-            <x-shows.dt>Created By</x-shows.dt>
-            <x-shows.dd
-                >{{ optional($role->created_by)->name ?? '-' }}
-            </x-shows.dd>
-        </x-shows.sub-dl>
-        @endrole @role('staff|super-admin')
-        <x-shows.sub-dl>
-            <x-shows.dt>Updated By</x-shows.dt>
-            <x-shows.dd
-                >{{ optional($role->approved_by)->name ?? '-' }}
-            </x-shows.dd>
-        </x-shows.sub-dl>
-        @endrole
-    </x-shows.dl>
-    @endif
+        {{-- <div class="py-2">
+            @foreach ($permissions as $permission)
+                <div>
+                    <x-inputs.checkbox id="permission{{ $permission->id }}" name="permissions[]"
+                        label="{{ ucfirst($permission->name) }}" value="{{ $permission->id }}" :checked="isset($role) ? $role->hasPermissionTo($permission) : false"
+                        :add-hidden-value="false"></x-inputs.checkbox>
+                </div>
+            @endforeach
+        </div> --}}
+    </div>
 </div>
--- resources/views/components/inputs/checkbox.blade.php
+++ resources/views/components/inputs/checkbox.blade.php
@@ -1,35 +1,22 @@
-@props([
-    'id',
-    'name',
-    'label',
-    'value',
-    'checked' => false,
-    'addHiddenValue' => true,
-    'hiddenValue' => 0,
-])
+@props(['id', 'name', 'label', 'value', 'checked' => false, 'addHiddenValue' => true, 'hiddenValue' => 0])
 
 @php
-    $checked = !! $checked
+$checked = !!$checked;
 @endphp
 
 <div class="relative block mb-2">
 
     {{-- Adds a hidden default value to be send if checked is false --}}
-    @if($addHiddenValue)
-    <input type="hidden" id="{{  $id ?? $name }}-hidden" name="{{ $name }}" value="{{ $hiddenValue }}">
+    @if ($addHiddenValue)
+        <input type="hidden" id="{{ $id ?? $name }}-hidden" name="{{ $name }}" value="{{ $hiddenValue }}">
     @endif
 
-    <input
-        type="checkbox"
-        id="{{ $id ?? $name }}"
-        name="{{ $name }}"
-        value="{{ $value ?? 1 }}"
+    <input type="checkbox" id="{{ $id ?? $name }}" name="{{ $name }}" value="{{ $value ?? 1 }}"
         {{ $checked ? 'checked' : '' }}
-        {{ $attributes->merge(['class' => '']) }}
-    >
+        {{ $attributes->merge(['class' => 'h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500']) }}>
 
-    @if($label ?? null)
-        <label class="text-gray-700 pl-2" for="{{ $id ?? $name }}">
+    @if ($label ?? null)
+        <label class="pl-2 text-gray-700" for="{{ $id ?? $name }}">
             {{ $label }}
         </label>
     @endif
