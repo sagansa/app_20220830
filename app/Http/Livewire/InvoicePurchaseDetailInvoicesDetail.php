54ef72773a9a517f7f8f2dae1a504bcb5ca16b5c
sagansa <asapanganbangsa@gmail.com>
2022-09-05 17:24:33 +0700

    Update InvoicePurchaseDetailInvoicesDetail.php

--- app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
+++ app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
@@ -55,8 +55,10 @@ class InvoicePurchaseDetailInvoicesDetail extends Component
 
         $this->detailRequestsForSelect = DetailRequest::
             join('request_purchases', 'request_purchases.id', '=', 'detail_requests.request_purchase_id')
+            ->join('products', 'products.id', '=', 'detail_requests.product_id')
+            ->where('products.payment_type_id', '=', '2')
+            ->whereIn('detail_requests.status', ['4', '5'])
             ->where('request_purchases.store_id', '=', $this->invoicePurchase->store_id)
-            ->whereIn('detail_requests.status', ['4,5'])
             ->get()->pluck('id', 'detail_request_name');
 
         $this->unitsForSelect = Unit::orderBy('unit', 'asc')->pluck('id', 'unit');
