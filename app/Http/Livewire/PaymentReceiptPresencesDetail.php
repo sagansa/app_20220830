42a0e168cab77e8f2ec5367172fef2b7e5de82f7
sagansa <asapanganbangsa@gmail.com>
2022-09-05 10:06:15 +0700

    Update PaymentReceiptPresencesDetail.php

--- app/Http/Livewire/PaymentReceiptPresencesDetail.php
+++ app/Http/Livewire/PaymentReceiptPresencesDetail.php
@@ -28,10 +28,10 @@ class PaymentReceiptPresencesDetail extends Component
     public function mount(PaymentReceipt $paymentReceipt)
     {
         $this->paymentReceipt = $paymentReceipt;
-        $this->presencesForSelect = Presence::join('closing_stores', 'presences.closing_store_id', '=', 'closing_stores.id')
-            ->orderBy('closing_stores.date', 'asc')
-            ->join('users', 'presences.created_by_id', '=', 'users.id')
+        $this->presencesForSelect = Presence::join('users', 'presences.created_by_id', '=', 'users.id')
             ->orderBy('users.name', 'asc')
+            ->join('closing_stores', 'presences.closing_store_id', '=', 'closing_stores.id')
+            ->orderBy('closing_stores.date', 'asc')
             ->get()
             ->where('payment_type_id', '=', '1')
             ->where('status', '=', '1')
