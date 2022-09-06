d69175f06822f235b68ecfeef26e2fdaebd8ecb3
sagansa <asapanganbangsa@gmail.com>
2022-09-06 09:54:12 +0700

    Update SalesOrderOnlinesList.php

--- app/Http/Livewire/SalesOrderOnlines/SalesOrderOnlinesList.php
+++ app/Http/Livewire/SalesOrderOnlines/SalesOrderOnlinesList.php
@@ -81,12 +81,12 @@ class SalesOrderOnlinesList extends Component
 
     public function getRowsQueryProperty()
     {
-        $salesOrderOnlines = SalesOrderOnline::orderBy('date', 'desc')
+        $salesOrderOnlines = SalesOrderOnline::orderBy('date', 'desc')->latest();
             // ->select(['sales_order_onlines.*', 'stores.name as storename'])
-            ->join('stores', 'stores.id', '=', 'sales_order_onlines.store_id')
+            // ->join('stores', 'stores.id', '=', 'sales_order_onlines.store_id')
             // ->join('customers', 'customers.id', '=', 'sales_order_onlines.customer_id')
-            ->join('online_shop_providers', 'online_shop_providers.id', '=', 'sales_order_onlines.online_shop_provider_id')
-            ->join('delivery_services', 'delivery_services.id', '=', 'sales_order_onlines.delivery_service_id');
+            // ->join('online_shop_providers', 'online_shop_providers.id', '=', 'sales_order_onlines.online_shop_provider_id')
+            // ->join('delivery_services', 'delivery_services.id', '=', 'sales_order_onlines.delivery_service_id');
 
         // $salesOrderOnlines = SalesOrderOnline::with('products')->query()->latest()
         //     ->map(function (SalesOrderOnline $products) {
