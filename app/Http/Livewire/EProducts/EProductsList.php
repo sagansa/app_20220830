c515ccc2c784033ed0f019c0c13f8271e2a8bbec
sagansa <asapanganbangsa@gmail.com>
2022-09-07 07:45:55 +0700

    Update EProductsList.php

--- app/Http/Livewire/EProducts/EProductsList.php
+++ app/Http/Livewire/EProducts/EProductsList.php
@@ -20,7 +20,7 @@ class EProductsList extends Component
 
     public function render()
     {
-        $eProducts = EProduct::paginate(16)->latest();
+        $eProducts = EProduct::paginate(16);
 
         return view('livewire.e-products.e-products-list', [
             'eProducts' => $eProducts,
