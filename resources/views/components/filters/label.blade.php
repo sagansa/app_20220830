1c9010703d6da075017ce688d298231f38686667
sagansa <asapanganbangsa@gmail.com>
2022-09-08 21:31:48 +0700

    update

--- resources/views/components/filters/label.blade.php
+++ resources/views/components/filters/label.blade.php
@@ -1,1 +1,1 @@
-<label class="block text-xs font-medium text-gray-700 sm:mt-px sm:pt-2">{{ $slot }}</label>
+<label class="block mt-2 text-xs font-medium text-gray-700 sm:mt-px sm:pt-2">{{ $slot }}</label>
--- resources/views/components/filters/select.blade.php
+++ resources/views/components/filters/select.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-1 sm:mt-0 sm:col-span-2">
     <select {{ $attributes }}
-        class="block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-xs">
+        class="block w-full max-w-lg text-xs border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs">
         <option value="">-- choose --</option>
         {{ $slot }}
     </select>
