bf0429109e58eea33143a9bdd4b7fe3e0358aa72
sagansa <asapanganbangsa@gmail.com>
2022-09-08 07:52:41 +0700

    Update production-production-main-froms-detail.blade.php

--- resources/views/livewire/production-production-main-froms-detail.blade.php
+++ resources/views/livewire/production-production-main-froms-detail.blade.php
@@ -1,20 +1,16 @@
 <div>
     <div>
         @can('create', App\Models\ProductionMainFrom::class)
-        <button class="button" wire:click="newProductionMainFrom">
-            <i class="mr-1 icon ion-md-add text-primary"></i>
-            @lang('crud.common.new')
-        </button>
-        @endcan @can('delete-any', App\Models\ProductionMainFrom::class)
-        <button
-            class="button button-danger"
-             {{ empty($selected) ? 'disabled' : '' }} 
-            onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
-            wire:click="destroySelected"
-        >
-            <i class="mr-1 icon ion-md-trash text-primary"></i>
-            @lang('crud.common.delete_selected')
-        </button>
+            <button class="button" wire:click="newProductionMainFrom">
+                <i class="mr-1 icon ion-md-add text-primary"></i>
+                @lang('crud.common.new')
+            </button>
+            @endcan @can('delete-any', App\Models\ProductionMainFrom::class)
+            <button class="button button-danger" {{ empty($selected) ? 'disabled' : '' }}
+                onclick="confirm('Are you sure?') || event.stopImmediatePropagation()" wire:click="destroySelected">
+                <i class="mr-1 icon ion-md-trash text-primary"></i>
+                @lang('crud.common.delete_selected')
+            </button>
         @endcan
     </div>
 
@@ -24,52 +20,37 @@
 
             <div class="mt-5">
                 <div>
-                    <x-input.select
-                        name="productionMainFrom.detail_invoice_id"
-                        label="Detail Invoice"
-                        wire:model="productionMainFrom.detail_invoice_id"
-                    >
+                    <x-input.select name="productionMainFrom.detail_invoice_id" label="Detail Invoice"
+                        wire:model="productionMainFrom.detail_invoice_id">
                         <option value="null" disabled>-- select --</option>
-                        @foreach($detailInvoicesForSelect as $value => $label)
-                        <option value="{{ $value }}"  >{{ $label }}</option>
+                        @foreach ($detailInvoicesForSelect as $label => $value)
+                            <option value="{{ $value }}">{{ $label }}</option>
                         @endforeach
                     </x-input.select>
                 </div>
             </div>
         </div>
 
-        <div class="px-6 py-4 bg-gray-50 flex justify-between">
-            <button
-                type="button"
-                class="button"
-                wire:click="$toggle('showingModal')"
-            >
+        <div class="flex justify-between px-6 py-4 bg-gray-50">
+            <button type="button" class="button" wire:click="$toggle('showingModal')">
                 <i class="mr-1 icon ion-md-close"></i>
                 @lang('crud.common.cancel')
             </button>
 
-            <button
-                type="button"
-                class="button button-primary"
-                wire:click="save"
-            >
+            <button type="button" class="button button-primary" wire:click="save">
                 <i class="mr-1 icon ion-md-save"></i>
                 @lang('crud.common.save')
             </button>
         </div>
     </x-modal>
 
-    <div class="block w-full overflow-auto scrolling-touch mt-4">
+    <div class="block w-full mt-4 overflow-auto scrolling-touch">
         <table class="w-full max-w-full mb-4 bg-transparent">
             <thead class="text-gray-700">
                 <tr>
-                    <th class="px-4 py-3 text-left w-1">
-                        <input
-                            type="checkbox"
-                            wire:model="allSelected"
-                            wire:click="toggleFullSelection"
-                            title="{{ trans('crud.common.select_all') }}"
-                        />
+                    <th class="w-1 px-4 py-3 text-left">
+                        <input type="checkbox" wire:model="allSelected" wire:click="toggleFullSelection"
+                            title="{{ trans('crud.common.select_all') }}" />
                     </th>
                     <th class="px-4 py-3 text-left">
                         @lang('crud.production_production_main_froms.inputs.detail_invoice_id')
@@ -79,42 +60,30 @@
             </thead>
             <tbody class="text-gray-600">
                 @foreach ($productionMainFroms as $productionMainFrom)
-                <tr class="hover:bg-gray-100">
-                    <td class="px-4 py-3 text-left">
-                        <input
-                            type="checkbox"
-                            value="{{ $productionMainFrom->id }}"
-                            wire:model="selected"
-                        />
-                    </td>
-                    <td class="px-4 py-3 text-left">
-                        {{ optional($productionMainFrom->detailInvoice)->id ??
-                        '-' }}
-                    </td>
-                    <td class="px-4 py-3 text-right" style="width: 134px;">
-                        <div
-                            role="group"
-                            aria-label="Row Actions"
-                            class="relative inline-flex align-middle"
-                        >
-                            @can('update', $productionMainFrom)
-                            <button
-                                type="button"
-                                class="button"
-                                wire:click="editProductionMainFrom({{ $productionMainFrom->id }})"
-                            >
-                                <i class="icon ion-md-create"></i>
-                            </button>
-                            @endcan
-                        </div>
-                    </td>
-                </tr>
+                    <tr class="hover:bg-gray-100">
+                        <td class="px-4 py-3 text-left">
+                            <input type="checkbox" value="{{ $productionMainFrom->id }}" wire:model="selected" />
+                        </td>
+                        <td class="px-4 py-3 text-left">
+                            {{ optional($productionMainFrom->detailInvoice)->id ?? '-' }}
+                        </td>
+                        <td class="px-4 py-3 text-right" style="width: 134px;">
+                            <div role="group" aria-label="Row Actions" class="relative inline-flex align-middle">
+                                @can('update', $productionMainFrom)
+                                    <button type="button" class="button"
+                                        wire:click="editProductionMainFrom({{ $productionMainFrom->id }})">
+                                        <i class="icon ion-md-create"></i>
+                                    </button>
+                                @endcan
+                            </div>
+                        </td>
+                    </tr>
                 @endforeach
             </tbody>
             <tfoot>
                 <tr>
                     <td colspan="2">
-                        <div class="mt-10 px-4">
+                        <div class="px-4 mt-10">
                             {{ $productionMainFroms->render() }}
                         </div>
                     </td>
