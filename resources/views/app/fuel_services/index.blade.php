10d818bf49d0339461fa5c93cf6c4bd364399d57
sagansa <asapanganbangsa@gmail.com>
2022-09-09 15:50:13 +0700

    Update index.blade.php

--- resources/views/app/fuel_services/index.blade.php
+++ resources/views/app/fuel_services/index.blade.php
@@ -85,9 +85,9 @@
                             <x-tables.td-right-hide>@currency($fuelService->amount / $fuelService->liter)
                             </x-tables.td-right-hide>
                         @endrole
-                        <x-tables.td-left-hide>{{ optional($fuelService->closingStore)->store->nickname ?? '-' }} -
-                            {{ optional($fuelService->closingStore)->shiftStore->name ?? '-' }} -
-                            {{-- {{ optional($fuelService->closingStore)->date->toFormattedDate() ?? '-' }} --}}
+                        <x-tables.td-left-hide>{{ optional($fuelService->closingStore)->store->nickname ?? '-' }} |
+                            {{ optional($fuelService->closingStore)->shiftStore->name ?? '-' }} |
+                            {{ optional($fuelService->closingStore)->date->toFormattedDate() ?? '-' }}
                         </x-tables.td-left-hide>
                         <td class="px-4 py-3 text-center" style="width: 134px;">
                             <div role="group" aria-label="Row Actions" class="relative inline-flex align-middle">
