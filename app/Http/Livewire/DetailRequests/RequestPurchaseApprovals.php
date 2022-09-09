f6389fd150701ef3bd695a39b6431f940b33bfeb
sagansa <asapanganbangsa@gmail.com>
2022-09-10 00:29:28 +0700

    update

---
+++ app/Http/Livewire/DetailRequests/RequestPurchaseApprovals.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Livewire\DetailRequests;
+
+use App\Http\Livewire\DataTables\WithBulkAction;
+use App\Http\Livewire\DataTables\WithCachedRows;
+use App\Http\Livewire\DataTables\WithFilter;
+use App\Http\Livewire\DataTables\WithModal;
+use App\Http\Livewire\DataTables\WithPerPagePagination;
+use App\Http\Livewire\DataTables\WithSortingDate;
+use Livewire\Component;
+
+class RequestPurchaseApprovals extends Component
+{
+     use WithPerPagePagination, WithSortingDate, WithModal, WithBulkAction, WithCachedRows, WithFilter;
+
+    public DetailRequest $editing;
+
+    public $sortColumn = 'detail_requests.created_at';
+
+    protected $queryString = [
+        'sortColumn' => [
+        'except' => 'detail_requests.date'
+        ],
+        'sortDirection' => [
+            'except' => 'asc',
+        ],
+    ];
+
+    public $filters = [
+        'status' => '',
+    ];
+
+    public function changeStatus(DetailRequest $detailRequest, $status)
+    {
+        Validator::make(['status' => $status], [
+			'status' => [
+				'required',
+				Rule::in(
+                    DetailRequest::STATUS_PROCESS,
+                    DetailRequest::STATUS_DONE,
+                    DetailRequest::STATUS_REJECT,
+                    DetailRequest::STATUS_APPROVED,
+                    DetailRequest::STATUS_NOT_VALID,
+                ),
+			],
+		])->validate();
+
+		$detailRequest->update(['status' => $status]);
+
+		$this->dispatchBrowserEvent('updated', ['message' => "Status changed to {$status} successfully."]);
+    }
+
+    public function getRowsQueryProperty()
+    {
+        $detailRequests = DetailRequest::query();
+
+        foreach ($this->filters as $filter => $value) {
+            if (!empty($value)) {
+                $detailRequests
+                    ->when($filter == 'status', fn($detailRequests) => $detailRequests->where('purchase_order_products.' . $filter, 'LIKE', '%' . $value . '%'));
+            }
+        }
+
+        return $this->applySorting($detailRequests);
+    }
+
+    public function getRowsProperty()
+    {
+        return $this->cache(function () {
+            return $this->applyPagination($this->rowsQuery);
+        });
+    }
+
+    public function render()
+    {
+        return view('livewire.detail-requests.request-purchase-approvals', [
+              'detailRequests' => $this->rows,
+        ]);
+    }
+}
---
+++ resources/views/livewire/detail-requests/request-purchase-approvals.blade.php
@@ -0,0 +1,117 @@
+<div>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Request Purchase Approvals
+        </h2>
+        <p class="mt-2 text-xs text-gray-700">persetujuan untuk pembelian bahan baku. Setelah bahan baku disetujui akan
+            tampil di INVOICE PURCHASE</p>
+    </x-slot>
+
+    <x-tables.topbar>
+        <x-slot name="search">
+            <x-buttons.link wire:click="$toggle('showFilters')">
+                @if ($showFilters)
+                    Hide
+                @endif Advanced Search...
+            </x-buttons.link>
+
+            @if ($showFilters)
+
+                <x-filters.group>
+                    <x-filters.label>Status</x-filters.label>
+                    <x-filters.select wire:model="filters.status">
+                        @foreach (App\Models\DetailRequest::STATUSES as $value => $label)
+                            <option value="{{ $value }}">{{ $label }}</option>
+                        @endforeach
+                    </x-filters.select>
+                </x-filters.group>
+
+                <x-buttons.link wire:click="resetFilters">Reset Filter
+                </x-buttons.link>
+
+            @endif
+        </x-slot>
+        <x-slot name="action">
+        </x-slot>
+    </x-tables.topbar>
+
+    <x-tables.card>
+        <x-table>
+            <x-slot name="head">
+                <tr>
+                    <x-tables.th-left>
+                        Product
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        Date
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        Store Request
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        Store Invoice
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        Quantity
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        Status
+                    </x-tables.th-left>
+
+                </tr>
+            </x-slot>
+            <x-slot name="body">
+                @foreach ($detailRequests as $detailRequest)
+                    <tr class="hover:bg-gray-100">
+                        <x-tables.td-left-hide>
+                            {{ $detailRequest->product->name }}
+                        </x-tables.td-left-hide>
+
+                        <x-tables.td-left-hide>
+                            {{ $detailRequest->requestPurchase->date->toFormattedDate() }}
+                        </x-tables.td-left-hide>
+
+                        <x-tables.td-left-hide>
+                            {{ optional($detailRequest->store)->nickname ?? '-' }}
+                        </x-tables.td-left-hide>
+
+                        <x-tables.td-left-hide>
+                            {{ optional($detailRequest->detailInvoice)->invoicePurchase->store->nickname ?? '-' }}
+                        </x-tables.td-left-hide>
+
+                        <x-tables.td-left-hide>
+                            {{ optional($detailRequest->detailInvoice)->quantity_product ?? '0' }}
+                            {{ optional($detailRequest->product)->unit->unit ?? '-' }}
+                        </x-tables.td-left-hide>
+
+                        <x-tables.td-left-hide>
+                            <select
+                                class="block w-full py-2 pl-3 pr-10 mt-1 text-xs border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                wire:change="changeStatus({{ $detailRequest }}, $event.target.value)">
+                                <option value="1" {{ $detailRequest->status == '1' ? 'selected' : '' }}>
+                                    Process</option>
+                                <option value="2" {{ $detailRequest->status == '2' ? 'selected' : '' }}>
+                                    Done</option>
+                                <option value="3" {{ $detailRequest->status == '3' ? 'selected' : '' }}>
+                                    Reject</option>
+                                <option value="4" {{ $detailRequest->status == '4' ? 'selected' : '' }}>
+                                    Approved</option>
+                                <option value="5" {{ $detailRequest->status == '5' ? 'selected' : '' }}>
+                                    Not Valid</option>
+                            </select>
+                        </x-tables.td-left-hide>
+                    </tr>
+                @endforeach
+            </x-slot>
+            <x-slot name="foot">
+                <tr>
+                    <td colspan="7">
+                        <div class="px-4 my-2">
+                            {{ $detailRequests->render() }}
+                        </div>
+                    </td>
+                </tr>
+            </x-slot>
+        </x-table>
+    </x-tables.card>
+</div>
