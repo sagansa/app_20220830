72d065d0f192c58137b544dfbaf64860ca8d2dcf
sagansa <asapanganbangsa@gmail.com>
2022-09-11 21:10:07 +0700

    Update ClosingStoreCashlessesDetail.php

--- app/Http/Livewire/ClosingStoreCashlessesDetail.php
+++ app/Http/Livewire/ClosingStoreCashlessesDetail.php
@@ -52,7 +52,7 @@ class ClosingStoreCashlessesDetail extends Component
         $this->closingStore = $closingStore;
         $this->accountCashlessesForSelect = AccountCashless::
             // orderBy('store_id', 'asc')
-            whereIn('store_id', $this->closingStore->store())
+            whereIn('store_id', $this->closingStore->store_id)
             ->get()
             ->pluck('id', 'account_cashless_name');
         $this->resetCashlessData();
