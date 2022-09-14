f8b0c184d64b2af4137dbbb32efe90e1c1a185b3
sagansa <asapanganbangsa@gmail.com>
2022-09-14 22:09:52 +0700

    update

--- resources/views/livewire/detail-invoices/check-productions.blade.php
+++ resources/views/livewire/detail-invoices/check-productions.blade.php
@@ -97,7 +97,7 @@
                         <x-tables.td-left-hide>
 
                             <select
-                                class="block w-full py-2 pl-3 pr-10 mt-1 text-xs border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                class="block w-full py-2 pl-3 pr-10 my-1 text-xs border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                                 wire:change="changeStatus({{ $detailInvoice }}, $event.target.value)">
                                 <option value="1" {{ $detailInvoice->status == '1' ? 'selected' : '' }}>
                                     Process</option>
