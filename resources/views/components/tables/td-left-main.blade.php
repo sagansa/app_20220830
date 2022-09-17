bb25157e746fe62c30b0216f398ef2f342065ba6
sagansa <asapanganbangsa@gmail.com>
2022-09-17 16:20:53 +0700

    Update td-left-main.blade.php

--- resources/views/components/tables/td-left-main.blade.php
+++ resources/views/components/tables/td-left-main.blade.php
@@ -6,7 +6,7 @@
     </div>
 </td> --}}
 
-<td class="pl-4 pr-3 text-xs sm:pl-6">
+<td class="pl-4 pr-3 my-1 text-xs  sm:pl-6">
     <div class="font-medium text-gray-900">{{ $main }}
     </div>
     <div class="flex flex-col mt-1 text-gray-500 sm:hidden">
