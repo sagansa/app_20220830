e62d6dc5bc55dd85776e386425c13bd7f86a29a6
sagansa <asapanganbangsa@gmail.com>
2022-09-18 00:50:57 +0700

    Update SalesOrderOnlinesList.php

--- app/Http/Livewire/SalesOrderOnlines/SalesOrderOnlinesList.php
+++ app/Http/Livewire/SalesOrderOnlines/SalesOrderOnlinesList.php
@@ -74,7 +74,7 @@ class SalesOrderOnlinesList extends Component
         $salesOrderOnlines = SalesOrderOnline::orderBy('date', 'desc')
             ->latest()
             ->paginate(10)
-            ->select('sales_order_onlines.*');
+            ->get();
 
         foreach ($this->filters as $filter => $value) {
                 if (!empty($value)) {
