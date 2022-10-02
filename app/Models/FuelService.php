ccb16ede7d5b22bf31db1a91701094539f0fab96
sagansa <asapanganbangsa@gmail.com>
2022-10-03 04:11:47 +0700

    Update FuelService.php

--- app/Models/FuelService.php
+++ app/Models/FuelService.php
@@ -82,6 +82,6 @@ class FuelService extends Model
 
     public function getFuelServiceNameAttribute()
     {
-        return $this->vehicle->no_register . ' | ' . $this->amount;
+        return $this->vehicle->no_register . ' | ' . $this->amount . ' | ' . $this->created_at . ' | ' . $this->created_by->name;
     }
 }
