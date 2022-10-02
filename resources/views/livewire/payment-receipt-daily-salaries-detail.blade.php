68ebde653d52df3ff6c813ca240edc52c566b779
sagansa <asapanganbangsa@gmail.com>
2022-10-02 10:51:31 +0700

    Update payment-receipt-daily-salaries-detail.blade.php

--- resources/views/livewire/payment-receipt-daily-salaries-detail.blade.php
+++ resources/views/livewire/payment-receipt-daily-salaries-detail.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div>
         @can('create', App\Models\DailySalary::class)
-        <button class="button" wire:click="newDailySalary">
-            <i class="mr-1 icon ion-md-add text-primary"></i>
-            @lang('crud.common.attach')
-        </button>
+            <button class="button" wire:click="newDailySalary">
+                <i class="mr-1 icon ion-md-add text-primary"></i>
+                @lang('crud.common.attach')
+            </button>
         @endcan
     </div>
 
@@ -12,91 +12,90 @@
         <div class="px-6 py-4">
             <div class="text-lg font-bold">{{ $modalTitle }}</div>
 
-            <div class="mt-5">
-                <div>
-                    <x-input.select
-                        name="daily_salary_id"
-                        label="Daily Salary"
-                        wire:model="daily_salary_id"
-                    >
-                        <option value="null" disabled>Please select the Daily Salary</option>
-                        @foreach($dailySalariesForSelect as $value => $label)
-                        <option value="{{ $value }}"  >{{ $label }}</option>
-                        @endforeach
-                    </x-input.select>
-                </div>
+            <div class="mt-1 sm:space-y-5">
+                <x-input.select name="daily_salary_id" label="Daily Salary" wire:model="daily_salary_id">
+                    <option value="null" disabled>Please select the Daily Salary</option>
+                    @foreach ($dailySalariesForSelect as $value => $label)
+                        <option value="{{ $value }}">{{ $label }}</option>
+                    @endforeach
+                </x-input.select>
             </div>
+
         </div>
 
-        <div class="px-6 py-4 bg-gray-50 flex justify-between">
-            <button
-                type="button"
-                class="button"
-                wire:click="$toggle('showingModal')"
-            >
-                <i class="mr-1 icon ion-md-close"></i>
-                @lang('crud.common.cancel')
-            </button>
-
-            <button
-                type="button"
-                class="button button-primary"
-                wire:click="save"
-            >
-                <i class="mr-1 icon ion-md-save"></i>
-                @lang('crud.common.save')
-            </button>
+        <div class="flex justify-between px-6 py-4 bg-gray-50">
+            <x-buttons.secondary wire:click="$toggle('showingModal')">Cancel</x-buttons.secondary>
+            <x-jet-button wire:click="save">Save</x-jet-button>
         </div>
     </x-modal>
 
-    <div class="block w-full overflow-auto scrolling-touch mt-4">
-        <table class="w-full max-w-full mb-4 bg-transparent">
-            <thead class="text-gray-700">
+    <x-tables.card-overflow>
+        <x-table>
+            <x-slot name="head">
                 <tr>
-                    <th class="px-4 py-3 text-left">
+                    <x-tables.th-left>
+                        date
+                    </x-tables.th-left>
+                    <x-tables.th-left>
                         @lang('crud.payment_receipt_daily_salaries.inputs.daily_salary_id')
-                    </th>
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        payment type
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        store
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        amount
+                    </x-tables.th-left>
+                    <x-tables.th-left>
+                        status
+                    </x-tables.th-left>
                     <th></th>
                 </tr>
-            </thead>
-            <tbody class="text-gray-600">
+            </x-slot>
+            <x-slot name="body">
                 @foreach ($paymentReceiptDailySalaries as $dailySalary)
-                <tr class="hover:bg-gray-100">
-                    <td class="px-4 py-3 text-left">
-                        {{ $dailySalary->date ?? '-' }}
-                    </td>
-                    <td class="px-4 py-3 text-right" style="width: 70px;">
-                        <div
-                            role="group"
-                            aria-label="Row Actions"
-                            class="relative inline-flex align-middle"
-                        >
-                            @can('delete-any', App\Models\DailySalary::class)
-                            <button
-                                class="button button-danger"
-                                onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
-                                wire:click="detach({{ $dailySalary->id }})"
-                            >
-                                <i
-                                    class="mr-1 icon ion-md-trash text-primary"
-                                ></i>
-                                @lang('crud.common.detach')
-                            </button>
-                            @endcan
-                        </div>
-                    </td>
-                </tr>
+                    <tr class="hover:bg-gray-100">
+                        <x-tables.td-left>
+                            {{ $dailySalary->date->toFormattedDate() ?? '-' }}
+                        </x-tables.td-left>
+                        <x-tables.td-left>
+                            {{ $dailySalary->created_by->name ?? '-' }}
+                        </x-tables.td-left>
+                        <x-tables.td-left>
+                            {{ $dailySalary->paymentType->name }}
+                        </x-tables.td-left>
+                        <x-tables.td-left>
+                            {{ $dailySalary->closingStore->store->nickname }}
+                        </x-tables.td-left>
+                        <x-tables.td-right>
+                            @currency($dailySalary->amount)
+                        </x-tables.td-right>
+                        <td class="px-4 py-3 text-right" style="width: 70px;">
+                            <div role="group" aria-label="Row Actions" class="relative inline-flex align-middle">
+                                @can('delete-any', App\Models\DailySalary::class)
+                                    <button class="button button-danger"
+                                        onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
+                                        wire:click="detach({{ $dailySalary->id }})">
+                                        <i class="mr-1 icon ion-md-trash text-primary"></i>
+                                        @lang('crud.common.detach')
+                                    </button>
+                                @endcan
+                            </div>
+                        </td>
+                    </tr>
                 @endforeach
-            </tbody>
-            <tfoot>
+            </x-slot>
+            <x-slot name="foot">
                 <tr>
                     <td colspan="2">
-                        <div class="mt-10 px-4">
+                        <div class="px-4 mt-10">
                             {{ $paymentReceiptDailySalaries->render() }}
                         </div>
                     </td>
                 </tr>
-            </tfoot>
-        </table>
-    </div>
+            </x-slot>
+        </x-table>
+    </x-tables.card-overflow>
 </div>
