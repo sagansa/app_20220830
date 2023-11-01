3ccf14098d04d4f897ed7f6aa8495c206ec0c21b
sagansa <asapanganbangsa@gmail.com>
2023-11-01 09:29:56 +0700

    Update InvoicePurchaseDetailInvoicesDetail.php

--- app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
+++ app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
@@ -50,7 +50,7 @@ class InvoicePurchaseDetailInvoicesDetail extends Component
         $this->detailRequestsForSelect = DetailRequest::with('products')
             ->where('store_id', $this->invoicePurchase->store_id)
             ->whereIn('status', ['4', '5'])
-            ->whereHas('products', function($query) {$query->where('payment_type_id', '=', $this->invoicePurchase->payment_type_id);})
+            // ->whereHas('products', function($query) {$query->where('payment_type_id', '=', $this->invoicePurchase->payment_type_id);})
             // ->where('payment_type_id', '=', '1')
             // ->orderBy('detail_request_name', 'desc')
             // ->where('request_purchase.date', '>=', Carbon::now()->subDays(7)->toDateString())
