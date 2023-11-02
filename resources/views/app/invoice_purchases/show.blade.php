f872fc0fa7d9a2d290a03749bb79253602e704af
sagansa <asapanganbangsa@gmail.com>
2023-11-02 22:13:48 +0700

    Update show.blade.php

--- resources/views/app/invoice_purchases/show.blade.php
+++ resources/views/app/invoice_purchases/show.blade.php
@@ -112,7 +112,12 @@
                 <x-partials.card class="mt-5">
                     <x-slot name="title"> Detail Invoices </x-slot>
 
-                    <livewire:invoice-purchase-detail-invoices-detail :invoicePurchase="$invoicePurchase" />
+                    @if ($invoicePurchase->payment_type_id == 2)
+                        <livewire:detail-invoices.detail-invoice-cash :invoicePurchase="$invoicePurchase" />
+                    @elseif ($invoicePurchase->payment_type_id == 1)
+                        <livewire:invoice-purchase-detail-invoices-detail :invoicePurchase="$invoicePurchase" />
+                    @endif
+
                 </x-partials.card>
             @endcan
 
