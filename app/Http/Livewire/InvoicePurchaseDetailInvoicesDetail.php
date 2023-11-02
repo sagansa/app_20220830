18bf89e3edd430cb488ddd74b6c59ac35c7c405a
sagansa <asapanganbangsa@gmail.com>
2023-11-02 10:11:43 +0700

    Update InvoicePurchaseDetailInvoicesDetail.php

--- app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
+++ app/Http/Livewire/InvoicePurchaseDetailInvoicesDetail.php
@@ -60,7 +60,7 @@ class InvoicePurchaseDetailInvoicesDetail extends Component
         //     ->pluck( 'id', 'detail_request_name');
 
         if ($this->invoicePurchase->payment_type_id = '1') {
-            $this->detailRequestsForSelect = DetailRequest::where('status', '1')
+            $this->detailRequestsForSelect = DetailRequest::where('status', '4')
                 ->where('store_id', $this->invoicePurchase->store_id)
                 ->get()
                 ->pluck('id', 'detail_request_name');
