c19144ce62be7318e179e467a1f9e785dd171de4
sagansa <asapanganbangsa@gmail.com>
2023-11-06 12:31:01 +0700

    Update unit-price-purchases.blade.php

--- resources/views/livewire/detail-invoices/unit-price-purchases.blade.php
+++ resources/views/livewire/detail-invoices/unit-price-purchases.blade.php
@@ -167,23 +167,26 @@
                         <x-tables.td-right-hide>
                             @currency($detailInvoice->subtotal_invoice)
                         </x-tables.td-right-hide>
-                        {{-- <td class="px-4 py-3 text-right" style="width: 134px;"> --}}
-                        <div role="group" aria-label="Row Actions" class="relative inline-flex align-middle">
-                            @can('update', $detailInvoice)
-                                {{-- <button type="button" class="button"
+                        <td>
+                            <div role="group" aria-label="Row Actions" class="relative inline-flex align-middle">
+                                @can('update', $detailInvoice)
+                                    {{-- <button type="button" class="button"
                                         wire:click="editDetailInvoice({{ $detailInvoice->id }})">
                                         <i class="icon ion-md-create"></i>
                                     </button> --}}
-                                <button type="button" class="w-4 mr-2 transform hover:text-yellow-500 hover:scale-110"
-                                    wire:click="editDetailInvoice({{ $detailInvoice->id }})">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </button>
-                            @endcan
-                        </div>
+                                    <button type="button" class="w-4 mr-2 transform hover:text-yellow-500 hover:scale-110"
+                                        wire:click="editDetailInvoice({{ $detailInvoice->id }})">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                        </svg>
+                                    </button>
+                                @endcan
+                            </div>
+                        </td>
+                        {{-- <td class="px-4 py-3 text-right" style="width: 134px;"> --}}
+
                         {{-- </td> --}}
                     </tr>
                 @endforeach
